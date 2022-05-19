927a699f798ea715454e4257793e110573630a64
socrams <82241613+socrams@users.noreply.github.com>
2022-05-19 12:32:16 -0300

    5

--- src/app/ahorcado/ahorcado/ahorcado.component.ts
+++ src/app/ahorcado/ahorcado/ahorcado.component.ts
@@ -1,41 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Ahorcado } from '../ahorcado';
-import { AhorcadoService } from '../services/ahorcado.service';
+import { Component, OnInit, ɵɵelementContainerEnd } from '@angular/core';
+import { Ahorcado } from '../class/ahorcado';
 
 @Component({
   selector: 'app-ahorcado',
   templateUrl: './ahorcado.component.html',
   styleUrls: ['./ahorcado.component.css']
 })
-
 export class AhorcadoComponent implements OnInit {
 
-public espacios:Array<string>;
-public letra:string = '';
-public miAhorcado: Ahorcado;
+  public espacios: Array <string>;
+  public letra: string = '';
+  public miAhorcado: Ahorcado;
 
-misNombrePais:Observable <Array<Ahorcado>>| undefined;
-miNombrePais: string = "";
-  constructor(public serviceAhoracado:AhorcadoService) {
-
+  constructor() { 
     this.miAhorcado = new Ahorcado();
-    this.miAhorcado.palabra = 'Encontrar';
+    this.miAhorcado.palabra = 'Encontrar'; // cambiar encontrar por array de palabras[x]
     this.espacios = this.miAhorcado.palabra.split('');
-
-
-   }
-
+  }
   ngOnInit(): void {
   }
-
-  validar (){
+  
+  validar() {
     if (/[a-zA-Z]$/.test(this.letra)) {
       this.miAhorcado.letrasElegidas += (',' +  this.letra )
-      this.letra=''; 
+      this.letra='';
     }
   }
   contiene(pal:string):boolean{
     return this.miAhorcado.letrasElegidas.toLocaleLowerCase().indexOf(pal.toLocaleLowerCase())>=0;
+  
   }
+  
+  
+
 }
