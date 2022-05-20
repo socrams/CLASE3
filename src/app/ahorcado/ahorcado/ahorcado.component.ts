8c2ed797d5ca60823bb9c01a5e12046bfd75b23b
socrams <82241613+socrams@users.noreply.github.com>
2022-05-20 16:28:22 -0300

    viernes

--- src/app/ahorcado/ahorcado/ahorcado.component.ts
+++ src/app/ahorcado/ahorcado/ahorcado.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit} from '@angular/core';
-import { Observable } from 'rxjs';
+//import { Observable } from 'rxjs'; 
 import { Ahorcado } from '../class/ahorcado';
-import { ListaPalabras } from '../class/lista-palabras';
+//import { ListaPalabras } from '../class/lista-palabras';
 import { AhorcadoService } from '../service/ahorcado.service';
+
 
 @Component({
   selector: 'app-ahorcado',
@@ -12,39 +13,47 @@ import { AhorcadoService } from '../service/ahorcado.service';
 export class AhorcadoComponent implements OnInit {
   public espacios: Array <string>;
   public letra: string = '';
+  public mensaje:string = '';
   public miAhorcado: Ahorcado;
-  miListaPalabras:Observable <Array<ListaPalabras>>;
-  //miListaPalabras:ListaPalabras[] = [];
+  //miListaPalabras:Observable <Array<ListaPalabras>>;
   miPalabra:string = "";
+  loqsea: Array<string> | undefined;
 
   constructor(public palabrasService:AhorcadoService) { 
-    this.miListaPalabras=palabrasService.getListaPalabras();
-    //this.palabrasService.getListaPalabras().subscribe(miListaPalabras=> this.miListaPalabras = miListaPalabras);
     this.miAhorcado = new Ahorcado();
+    // this.miListaPalabras=palabrasService.getListaPalabras();
     this.miAhorcado.palabra = 'Encontrar'; // cambiar encontrar por array de palabras[x]
     this.espacios = this.miAhorcado.palabra.split('');
     
-  }
+  } 
   ngOnInit(): void {
+    this.palabrasService.getListaPalabras()
+    .subscribe(resp=> {
+      console.log(resp);
+    }) 
+
   }
   
   validar() {
     if (/[a-zA-Z]$/.test(this.letra)) {
       this.miAhorcado.letrasElegidas += (', ' +  this.letra )
-      this.letra='';
-      
+      this.letra=''; 
+      this.mensaje=''; 
+    }
+    else {
+      this.letra= "";
+      this.mensaje = "Debe ingresar solo letras";
     }
   }
   contiene(pal:string):boolean{
     return this.miAhorcado.letrasElegidas.toLocaleLowerCase().indexOf(pal.toLocaleLowerCase())>=0;
-  
   }
-   
   
-  mostrar(lista:Array<1>){
+  boton () {
+      //https://www.youtube.com/watch?v=J2tN5zG0k18
+     
+  }
 
-    console.log(lista);
-  }
 }
 
 
