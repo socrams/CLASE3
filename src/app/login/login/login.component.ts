4134704ddd695ed0c9e8e9070c08895b319890c1
socrams <82241613+socrams@users.noreply.github.com>
2022-05-24 13:46:56 -0300

    actualizar version

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -6,9 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+<<<<<<< Updated upstream
+=======
+//public form: FormGroup;
+>>>>>>> Stashed changes
 
   constructor() { }
 
+<<<<<<< Updated upstream
+=======
+  constructor() { 
+    //public fb:FormBuilder
+    // this.form=fb.group({
+    //   'nombre':['' ,[Validators.required],[Validators.min(5)]],
+    //   'password':['',[Validators.required],]
+    // })
+  }
+  // public loguear(){
+  //   this.form.getRawValue();
+    
+  // }
+    
+>>>>>>> Stashed changes
   ngOnInit(): void {
   }
 
