01b3cc728347d6d286cdc6f9278081bd5195b496
socrams <82241613+socrams@users.noreply.github.com>
2022-05-30 19:37:07 -0300

    as

--- src/app/ahorcado/ahorcado/ahorcado.component.ts
+++ src/app/ahorcado/ahorcado/ahorcado.component.ts
@@ -9,33 +9,24 @@ import { AhorcadoService } from '../service/ahorcado.service';
   styleUrls: ['./ahorcado.component.css']
 })
 export class AhorcadoComponent implements OnInit  {
-  public espacios: Array <string>;
+  public espacios: Array <string>=[];
   public letra: string = '';
   public mensaje:string = '';
   public x: number= 0;
   public miAhorcado: Ahorcado;
   public palabra: any;
-
+  
 
   constructor(public palabrasService:AhorcadoService) {
     this.miAhorcado = new Ahorcado();
-    //this.palabra = this.palabrasService.getListaPalabras()
-    // .subscribe(resp => {
-    //   (resp[this.x= Math.floor(Math.random()*250)].name.common);
-    // });
-    //this.palabra= this.palabrasService.getRamdomWord();
-
-    //this.espacios = new Array(this.palabra.length);
-
-    this.miAhorcado.palabra = 'Encontrar';
-    this.espacios = this.miAhorcado.palabra.split('');
-
-
-
-
-
-
-
+    this.palabrasService.getListaPalabras()
+    .subscribe(resp => {
+         this.miAhorcado.palabra=(resp[this.x = Math.floor(Math.random()*250)].name.common);
+      
+      // console.log(
+      //   this.miAhorcado.palabra);
+        this.espacios = this.miAhorcado.palabra.split('');
+     });
   }
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
@@ -57,17 +48,5 @@ export class AhorcadoComponent implements OnInit  {
   contiene(pal:string):boolean{
     return this.miAhorcado.letrasElegidas.toLocaleLowerCase().indexOf(pal.toLocaleLowerCase())>=0;
   }
-
-  boton () { //esto va al ctor.
-    this.palabrasService.getListaPalabras()
-    .subscribe(resp => {
-      console.log(resp[0].name.common);
-    })
-
-  }
-
-
-  getRamdomValue(){
-    return this.x= Math.floor(Math.random()*250);
-  }
+ 
 }
