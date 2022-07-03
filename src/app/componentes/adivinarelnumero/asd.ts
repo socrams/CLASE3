83309cb67391b3c3d1263dd40c39a03749e41185
socrams <82241613+socrams@users.noreply.github.com>
2022-07-03 12:19:05 -0300

    tabla

--- src/app/componentes/adivinarelnumero/asd.ts
+++ src/app/componentes/adivinarelnumero/asd.ts
@@ -1,15 +1,15 @@
-let nombreUsuarioLogeado = this.loginservice.getUsuarioLogeado();
-      this.posicion= this.todosLosUsuarios.findIndex((t:any)=>t.usuario==nombreUsuarioLogeado);
-      //console.log(this.posicion);
-      this.todosLosUsuarios[this.posicion].puntaje1=this.todosLosUsuarios[this.posicion].puntaje1+5;
-      //console.log(this.todosLosUsuarios);
-      localStorage.setItem("Usuarios",JSON.stringify(this.todosLosUsuarios));
+// let nombreUsuarioLogeado = this.loginservice.getUsuarioLogeado();
+//       this.posicion= this.todosLosUsuarios.findIndex((t:any)=>t.usuario==nombreUsuarioLogeado);
+//       //console.log(this.posicion);
+//       this.todosLosUsuarios[this.posicion].puntaje1=this.todosLosUsuarios[this.posicion].puntaje1+5;
+//       //console.log(this.todosLosUsuarios);
+//       localStorage.setItem("Usuarios",JSON.stringify(this.todosLosUsuarios));
 
 
 
-      this.todosLosUsuarios = JSON.parse(localStorage.getItem("Usuarios")??"[]");
+//       this.todosLosUsuarios = JSON.parse(localStorage.getItem("Usuarios")??"[]");
 
 
 
-      todosLosUsuarios:Array<Usuario>
-      posicion :any ;
+//       todosLosUsuarios:Array<Usuario>
+//       posicion :any ;
