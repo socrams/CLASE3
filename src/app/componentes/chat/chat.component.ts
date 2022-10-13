c22e3360fc6af09611da1d71918148a33b722870
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2022-10-13 14:16:44 -0300

    s

--- src/app/componentes/chat/chat.component.ts
+++ src/app/componentes/chat/chat.component.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
+import {createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-chat',
@@ -7,8 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  titleInput: string ="";
+  messages: string ="";
+  lists: any[] = [];
+  x : any;
+  supabase:SupabaseClient;
+  
+  constructor() { 
+    this.supabase = createClient(environment.supabase.supabaseUrl, environment.supabase.supabaseKey)
+  }
+  enviarPost(){
 
-  constructor() { }
+}
+
+async rellenarChat(){
+  let { data: menssages } = await this.supabase
+  .from('menssages')
+  .select('content')
+  
+  console.log('mensajes: ', menssages);
+  this.x= data.menssages;
+  
+}
+
+enviarMensaje(){
+
+}
 
   ngOnInit(): void {
   }
